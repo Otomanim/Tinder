21b30675d4b960ce07717988aa18776ae13b7c13
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2021-11-13 13:45:18 -0300

    Update CombineVC
    add múltiplos cards e rotação neles de forma animada

--- Tinder/Controller/CombineVC.swift
+++ Tinder/Controller/CombineVC.swift
@@ -14,18 +14,27 @@ class CombineVC: UIViewController {
         
         view.backgroundColor = UIColor.systemGroupedBackground
         
-        let redView = UIView()
-        redView.backgroundColor = .red
-        redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+        self.adicionarCards()
+    }
+}
+extension CombineVC {
+    func adicionarCards () {
         
-        redView.center = view.center
+        for item in 1...3 {
+            let redView = UIView()
+            redView.backgroundColor = item == 2 ? .blue : .red
+            redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+            
+            redView.center = view.center
+            
+            let gesture = UIPanGestureRecognizer()
+            gesture.addTarget(self, action: #selector(handlerCard))
+            
+            redView.addGestureRecognizer(gesture)
+            
+            view.addSubview(redView)
+        }
         
-        let gesture = UIPanGestureRecognizer()
-        gesture.addTarget(self, action: #selector(handlerCard))
-        
-        redView.addGestureRecognizer(gesture)
-        
-        view.addSubview(redView)
     }
 }
 
@@ -36,10 +45,14 @@ extension CombineVC {
             
             card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
             
+            let rotationAngle = point.x / view.bounds.width * 0.4
+            
+            card.transform = CGAffineTransform(rotationAngle: rotationAngle)
             if gesture.state == .ended {
                 
                 UIView.animate(withDuration: 0.5){
                     card.center = self.view.center
+                    card.transform = .identity
                 }
                 
             }
